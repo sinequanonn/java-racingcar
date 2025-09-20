7a39f99139ccd2a77056b38db4a925c692035db6
sinequanonn <dleck288@gmail.com>
2025-09-21 00:04:13 +0900

    feat: implement print message

--- src/main/java/racingcar/util/MessageUtil.java
+++ src/main/java/racingcar/util/MessageUtil.java
@@ -4,7 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class MessageUtil {
 
-    void MainMessage() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    void printMessage(String message) {
+        System.out.println(message);
     }
+
 }
