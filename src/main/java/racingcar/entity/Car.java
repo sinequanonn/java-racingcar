c523525f1889426fc3cb9a2f87f0e9eeee76cbcf
sinequanonn <dleck288@gmail.com>
2025-09-21 00:47:19 +0900

    feat: implement entire flow

--- src/main/java/racingcar/entity/Car.java
+++ src/main/java/racingcar/entity/Car.java
@@ -3,4 +3,25 @@ package racingcar.entity;
 public class Car {
     private String name;
     private Integer move;
+
+    public Car(String name, Integer move) {
+        this.name = name;
+        this.move = move;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMove(Integer move) {
+        this.move = move;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getMove() {
+        return move;
+    }
 }
