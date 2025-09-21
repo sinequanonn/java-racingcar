35c4b248fc2f46f61dbc71e7ead4516bdf832c1a
sinequanonn <dleck288@gmail.com>
2025-09-21 21:17:40 +0900

    Feat: implement input method.
    경주 할 자동차 이름 입력 구현
    시도할 횟수 입력 구현

--- src/main/java/racingcar/util/InputUtil.java
+++ src/main/java/racingcar/util/InputUtil.java
@@ -1,4 +1,30 @@
 package racingcar.util;
 
+import racingcar.entity.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputUtil {
+
+    public List<Car> getCarNames() {
+        String line = Console.readLine();
+        String[] carNames = line.split("\\s*, \\s*");
+
+        List<Car> cars = new ArrayList<>();
+
+        for (String car : carNames) {
+            String trimmedCar = car.trim();
+
+            cars.add(new Car(trimmedCar));
+        }
+        return cars;
+    }
+
+    public int getCount() {
+        int count = Integer.parseInt(Console.readLine());
+
+        return count;
+    }
 }
