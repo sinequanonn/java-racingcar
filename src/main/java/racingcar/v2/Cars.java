1adad48c20963e7a7c3c673023ae2b2fc122309f
sinequanonn <dleck288@gmail.com>
2025-09-24 17:03:01 +0900

    feat: implement Car

--- src/main/java/racingcar/v2/Cars.java
+++ src/main/java/racingcar/v2/Cars.java
@@ -1,11 +1,12 @@
 package racingcar.v2;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
 
-    private List<Car> cars;
+    private List<Car> cars = new ArrayList<>();
 
     public Cars(List<String> carNames) {
         this.cars = carNames.stream()
