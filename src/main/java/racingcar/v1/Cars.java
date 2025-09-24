9162e978562dffbbd56d1270081a2fbc9b682db9
sinequanonn <dleck288@gmail.com>
2025-09-24 20:11:42 +0900

    feat: implement Cars.java
    자동차 일급 객체 구현
    자동차 입력을 받아, 자동차 리스트를 생성

---
+++ src/main/java/racingcar/v1/Cars.java
@@ -0,0 +1,35 @@
+package racingcar.v1;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars = new ArrayList<>();
+
+    public void playRace() {
+        createCars();
+    }
+
+    private void createCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)");
+        String carInput = Console.readLine();
+        List<String> carNames = getCarNames(carInput);
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
+    private List<String> getCarNames(String input) {
+        List<String> carNames = new ArrayList<>();
+        String[] splitInput = input.split(",");
+
+        for (String name : splitInput) {
+            carNames.add(name);
+        }
+        return carNames;
+    }
+
+
+}
