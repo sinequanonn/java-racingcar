5f4697d12320631f28487a5dc70ccab4b93e1113
sinequanonn <dleck288@gmail.com>
2025-09-24 20:26:14 +0900

    feat: implement Car and ViewRound
    1라운드 게임 진행하는 로직 구현
    1라운드 후 중간 결과 출력하는 기능 구현

---
+++ src/main/java/racingcar/v1/Car.java
@@ -0,0 +1,36 @@
+package racingcar.v1;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private String name;
+    private int move;
+
+    public Car(String name) {
+        validate(name);
+        this.name = name;
+        this.move = 0;
+    }
+
+    public void playRandomMove() {
+        int random = Randoms.pickNumberInRange(0, 9);
+
+        if (random >= 4) {
+            this.move += 1;
+        }
+    }
+
+    public void printRound() {
+        System.out.println(this.name + " : " + "-".repeat(move));
+    }
+
+    private void validate(String name) {
+        if (name.length() >= 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
