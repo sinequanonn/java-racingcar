60d6333ba0cc6f177436ffbfa820a6be6ed56499
sinequanonn <dleck288@gmail.com>
2025-09-24 20:23:31 +0900

    Test: validate Cars.java
    자동차 입력을 받아, 자동차 리스트를 생성 테스트

---
+++ src/test/java/racingcar/v1/CarsTest.java
@@ -0,0 +1,49 @@
+package racingcar.v1;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarsTest {
+
+    Cars cars = new Cars();
+
+    @BeforeEach
+    @AfterEach
+    void set() {
+        Console.close();
+    }
+
+    @Test
+    void 자동차_리스트_생성() {
+        // given
+        String input = "pobi, jun, won\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        List<Car> cars1 = cars.getCars();
+        cars.playRace();
+        assertThat(cars1.get(0).getName()).isEqualTo("pobi");
+        assertThat(cars1.get(1).getName()).isEqualTo("jun");
+        assertThat(cars1.get(2).getName()).isEqualTo("won");
+    }
+
+    @Test
+    void 자동차_리스트_생성_예외처리() {
+        String input = "pobi123, jun, won\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        List<Car> cars1 = cars.getCars();
+        assertThatThrownBy(() -> cars.playRace())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
