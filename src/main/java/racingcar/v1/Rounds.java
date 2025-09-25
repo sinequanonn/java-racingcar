13139f2a2737b3272cec74852c74ccaf0c8eac0f
sinequanonn <dleck288@gmail.com>
2025-09-25 10:06:08 +0900

    rename: change method name
    Rounds의 메소드 이름 변경

--- src/main/java/racingcar/v1/Rounds.java
+++ src/main/java/racingcar/v1/Rounds.java
@@ -4,15 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
-public class Round {
+public class Rounds {
 
     private int round;
 
-    public Round() {
-        this.round = createRound();;
+    public Rounds() {
+        this.round = createRound();
     }
 
-    public void roundPlay(List<Car> cars) {
+    public void roundAllPlay(List<Car> cars) {
         for (int i = 0; i < this.round; i++) {
             playOneGame(cars);
             ViewRound.printOneRoundResult(cars);
